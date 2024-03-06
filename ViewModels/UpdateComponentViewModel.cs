59196c7fce9752f60db91eecec2218ff199b014f
Naywia <48431873+Naywia@users.noreply.github.com>
2024-03-06 17:53:05 +0100

    Almost finished implementation for Use Case 6
    Use Case 6: Sæt serviceinterval for Komponent

--- ViewModels/UpdateComponentViewModel.cs
+++ ViewModels/UpdateComponentViewModel.cs
@@ -1,26 +1,36 @@
 ﻿using Hydac.Models;
+using Hydac.Persistence;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Hydac.ViewModels
 {
     public class UpdateComponentViewModel
     {
         #region Fields
+        private ComponentRepo componentRepo = App.ComponentRepo;
         private ICommand updateServiceIntervalCommand;
+        public ObservableCollection<Component> Components { get; set; }
         #endregion
 
         #region Constructors
+        public UpdateComponentViewModel()
+        {
+            Components = componentRepo.GetItems();
+            Component = Components.FirstOrDefault();
+        }
         #endregion
 
         #region Methods
         private void UpdateServiceInterval()
         {
-
+            componentRepo.UpdateServiceInterval(Component);
         }
         #endregion
 
@@ -37,5 +47,9 @@ namespace Hydac.ViewModels
             }
         }
         #endregion
+
+
+
+
     }
 }
